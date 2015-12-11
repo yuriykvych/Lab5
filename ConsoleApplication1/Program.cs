315bfa9a5f9aef2c2a442af42b6830dc3c0165b6
Юрій Квич < >
2015-12-11 09:52:29 +0200

    4 commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApplication1
             String s = Console.ReadLine();
             s = "Hello, " + s + ".";
             Console.WriteLine(s);
+            Console.WriteLine("asdasd");
             Console.ReadLine();
         }
     }
